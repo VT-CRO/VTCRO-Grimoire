809685d98e63aa216b7020916bd204bcd8f1349f
Devyn04 <maskofgerma@gmail.com>
2023-10-22 13:11:31 -0400

    Better implemented the QTR_init function

--- QTR_8/QTR_8.cpp
+++ QTR_8/QTR_8.cpp
@@ -1,8 +1,7 @@
-#include <arduino_freertos.h>;
 typedef struct
 {
-  int pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8;
-  uint8_t* data[8];
+  uint16_t pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8;
+  uint16_t* data;
 } QTR_t;
 
 int s0 = A0;
@@ -13,16 +12,27 @@ int s4 = A4;
 int s5 = A5;
 int s6 = A6;
 int s7 = A7;
-uint8_t readings[8];
+uint16_t readings[8];
+QTR_t data{};
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  //QTR_t values = QTR_init(s0, s1, s2, s3, s4, s5, s6, s7, readings);
+  data = QTR_init(s0, s1, s2, s3, s4, s5, s6, s7, readings);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  
+  data.data[0] = analogRead(data.pin1);
+  data.data[1] = analogRead(data.pin2);
+  data.data[2] = analogRead(data.pin3);
+  data.data[3] = analogRead(data.pin4);
+  data.data[4] = analogRead(data.pin5);
+  data.data[5] = analogRead(data.pin6);
+  data.data[6] = analogRead(data.pin7);
+  data.data[7] = analogRead(data.pin8);
+/*
   readings[0] = analogRead(s0);
   readings[1] = analogRead(s1);
   readings[2] = analogRead(s2);
@@ -31,16 +41,26 @@ void loop() {
   readings[5] = analogRead(s5);
   readings[6] = analogRead(s6);
   readings[7] = analogRead(s7);
-
+*/
   for (uint8_t i = 0; i < 8; i++)
   {
     Serial.print("Value at ");
     Serial.print(i);
     Serial.print(": ");
-    Serial.println(readings[i]);
+    Serial.println(data.data[i]);
     
   }
 
+  data.data[0] = changeBit(data.data[0]);
+  data.data[1] = changeBit(data.data[1]);
+  data.data[2] = changeBit(data.data[2]);
+  data.data[3] = changeBit(data.data[3]);
+  data.data[4] = changeBit(data.data[4]);
+  data.data[5] = changeBit(data.data[5]);
+  data.data[6] = changeBit(data.data[6]);
+  data.data[7] = changeBit(data.data[7]);
+
+/*
   readings[0] = changeBit(readings[0]);
   readings[1] = changeBit(readings[1]);
   readings[2] = changeBit(readings[2]);
@@ -49,22 +69,23 @@ void loop() {
   readings[5] = changeBit(readings[5]);
   readings[6] = changeBit(readings[6]);
   readings[7] = changeBit(readings[7]);
+*/
   for (uint8_t j = 0; j < 8; j++)
   {
     Serial.print("Value at ");
     Serial.print(j);
     Serial.print(": ");
-    Serial.println(readings[j]);
+    Serial.println(data.data[j]);
     
   }
-
+  
   delay(1000);
 }
 
 
-QTR_t QTR_init(int pin1, int pin2, int pin3, int pin4, int pin5, int pin6, int pin7, int pin8, uint8_t* data)
+QTR_t QTR_init(uint16_t pin1, uint16_t pin2, uint16_t pin3, uint16_t pin4, uint16_t pin5, uint16_t pin6, uint16_t pin7, uint16_t pin8, uint16_t data[])
 {
-   QTR_t values{pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, data};
+   QTR_t values = {pin1, pin2, pin3, pin4, pin5, pin6, pin7, pin8, data};
    return values;
 }
 
